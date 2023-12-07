32a486ca48a881b3a03b6914c0fcca85ebf791e5
Rawisha <rawvsmaw22@gmail.com>
2023-12-07 12:32:50 +0100

    auth added

---
+++ src/collections/Users.ts
@@ -0,0 +1,26 @@
+import { CollectionConfig } from "payload/types";
+
+export const Users: CollectionConfig = {
+  slug: "users",
+  auth: {
+    verify: {
+      generateEmailHTML: ({ token }) => {
+        return `´<a href='${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}'>Verify account</a>`;
+      },
+    },
+  },
+  access: {
+    read: () => true,
+    create: () => true,
+  },
+  fields: [
+    {
+      name: "role",
+      type: "select",
+      options: [
+        { label: "Admin", value: "admin" },
+        { label: "User", value: "user" },
+      ],
+    },
+  ],
+};
---
+++ src/get-payload.ts
@@ -0,0 +1,65 @@
+import dotenv from "dotenv";
+import path from "path";
+import payload, { Payload } from "payload";
+import type { InitOptions } from "payload/config";
+import nodemailer from "nodemailer";
+
+dotenv.config({
+  path: path.resolve(__dirname, "../.env"),
+});
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  secure: true,
+  port: 465,
+  auth: {
+    user: "rawvsmaw22@gmail.com",
+    pass: process.env.GOOGLE_CLIENT_SECRET,
+  },
+});
+
+let cached = (global as any).payload;
+
+if (!cached) {
+  cached = (global as any).payload = {
+    client: null,
+    promise: null,
+  };
+}
+
+interface Args {
+  initOptions?: Partial<InitOptions>;
+}
+export const getPayloyadClient = async ({
+  initOptions,
+}: Args = {}): Promise<Payload> => {
+  if (!process.env.PAYLOAD_SECRET) {
+    throw new Error("PAYLOAD_SECRET is missing");
+  }
+
+  if (cached.client) {
+    return cached.client;
+  }
+
+  if (!cached.promise) {
+    cached.promise = payload.init({
+      email: {
+        transport: transporter,
+        fromAddress: "rawvsmaw22@gmail.com",
+        fromName: "DigitalHippo",
+      },
+      secret: process.env.PAYLOAD_SECRET,
+      local: initOptions?.express ? false : true,
+      ...(initOptions || {}),
+    });
+  }
+
+  try {
+    cached.client = await cached.promise;
+  } catch (e: unknown) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.client;
+};
